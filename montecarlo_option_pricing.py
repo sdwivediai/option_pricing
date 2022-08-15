13543972278ee6a47de5498acd42b1045dd2fa59
sdwivediai <68904175+sdwivediai@users.noreply.github.com>
2022-08-15 17:07:07 +0530

    Update montecarlo_option_pricing.py

--- montecarlo_option_pricing.py
+++ montecarlo_option_pricing.py
@@ -7,7 +7,7 @@ with st.sidebar:
     
     stock_price = st.number_input('Enter stock price', min_value=0.1, step=0.1)
     strike_price = st.number_input('Enter strike price', min_value=0, step=20)
-    days_to_expire = st.slider('Number of days to expire', min_value=1, max_value=30, step=1)
+    days_to_expire = st.slider('Number of days to expire', value=1, min_value=1, max_value=30, step=1)
     interest_rate = st.number_input('Risk Free interest rate')
     hist_vol = st.number_input('Hist vol', min_value=0.01, step=0.001)
     num_reps = st.number_input('Enter number of reps', min_value=1000, step=500)
@@ -31,5 +31,6 @@ def mc_euro_options(option_type,stock_price,strike,maturity,interest_rate,hist_v
 
 option_premium = mc_euro_options(option_type,stock_price,strike_price,maturity,interest_rate,hist_vol,num_reps)  
 
-st.write('Option Premium according to Monte Carlo method:'+ option_premium)
+st.write('Option Premium according to Monte Carlo method:')
+st.write(option_premium)
  
